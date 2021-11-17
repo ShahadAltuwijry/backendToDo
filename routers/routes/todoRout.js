c2aba712b920e261e99234037c9958e70eeb8929
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-11-17 04:06:33 +0300

    added a starter post router

--- routers/routes/todoRout.js
+++ routers/routes/todoRout.js
@@ -1,7 +1,17 @@
+const { application } = require("express");
 
 const toDoList = [
-  { id: 1, task: "sleep", isComplete: false },
-  { id: 2, task: "eat", isComplete: false },
-  { id: 3, task: "sleep", isComplete: false },
+  { id: 0, task: "sleep", isComplete: false },
+  { id: 1, task: "eat", isComplete: false },
+  { id: 2, task: "sleep", isComplete: false },
 ];
 
+// const addToDo = (req, res) => {
+//     let newTask = {id: toDoList.length, name: }
+// }
+
+app.post("/addtask", (req, res) => {
+    const {id, name, isComplete} = req.body;
+    toDoList.push({id: toDoList.length, name: req.body, isComplete: false});
+    res.status(201).json({id, name, isComplete});
+});
