2ac8806863f7ad227bae438f178382a62425cc0d
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-11-17 05:30:39 +0300

    finished debugging

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
+const morgan = require("morgan");
 const mainRouter = require("./routers/routes/todoRout");
-dotenv.config();
 
 const app = express();
 
-app.use(express.json());
 app.use("/todolist", mainRouter);
+
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
 
 const PORT = process.env.PORT || 5000;
 
